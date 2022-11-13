2b8be9831e70a41b84af9c6a98f58b3bbd152b6c
Jahidul Hasan Hemal <jahidulhasanhemal@gmail.com>
2022-11-13 00:20:23 -0500

    Initial commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Jahidul Hasan Hemal | Software Engineer | Data Scientiest </title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Jahidul Hasan Hemal - Software Engineer | Google Developer Expert | " />
+            <meta name="description"
+                content="Jahidul Hasan Hemal's (jhhemal) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS. Software Engineer, Google Developer Expert" />
+            <meta name="author" content="Jahidul Hasan (jhhemal)" />
+            <meta name="keywords"
+                content="jhhemal, jhhemal's portfolio, jhhemal linux, ubuntu portfolio, Jahidul Hasan Hemal protfolio,Jahidul Hasan Hemal computer, Jahidul Hasan Hemal, jhhemal ubuntu, Jahidul Hasan Hemal ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Jahidul Hasan Hemal Portfolio - Software Engineer | Google Developer Expert" />
+            <meta itemProp="description"
+                content="Jahidul Hasan Hemal's (jhhemal) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS. Software Engineer, Google Developer Expert." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Jahidul Hasan Hemal - Software Engineer | Google Developer Expert" />
+            <meta name="twitter:description"
+                content="Jahidul Hasan Hemal's (jhhemal) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS. Software Engineer, Google Developer Expert" />
+            <meta name="twitter:site" content="jhhemal" />
+            <meta name="twitter:creator" content="jhhemal" />
+            <meta name="twitter:image:src" content="images/logos/logos.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Jahidul Hasan Hemal - Software Engineer | Google Developer Expert" />
+            <meta name="og:description"
+                content="Jahidul Hasan Hemal's (jhhemal) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS. Software Engineer, Google Developer Expert." />
+            <meta name="og:image" content="images/logos/logos.png" />
+            <meta name="og:url" content="http://jhhemal.dev/" />
+            <meta name="og:site_name" content="Jahidul Hasan Hemal Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.png" />
+            <link rel="apple-touch-icon" href="images/logos/favicon.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
