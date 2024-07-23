17411c0eded476758a9ca66de89247162ee768fe
Jahidul Hasan Hemal <jahidulhasanhemal@gmail.com>
2024-07-23 07:09:37 -0400

    Add middleware and terminal response4

--- middleware.js
+++ middleware.js
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-  const url = req.nextUrl.clone();
-  url.pathname = '/api/terminal';
-  return NextResponse.rewrite(url);
+  const userAgent = req.headers.get('user-agent') || '';
+
+  if (userAgent.includes('curl') || userAgent.includes('wget')) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/api/terminal';
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
 }
