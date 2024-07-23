789d494d1711718b1dfdd8ecb590a005317f3186
Jahidul Hasan Hemal <jahidulhasanhemal@gmail.com>
2024-07-23 06:39:13 -0400

    next.config updated

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'user-agent',
-            value: '(curl|wget)', // Regex to match curl or wget
-          },
-        ],
-        destination: '/middleware',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
